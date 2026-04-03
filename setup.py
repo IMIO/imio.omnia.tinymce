b684cc0c6b5c6e2192579d03f5245b84118c0f8a
Antoine Duchêne <antoine.duchene@imio.be>
2026-04-03 17:52:27 +0200

    Back to development: 1.0a4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='imio.omnia.tinymce',
-    version='1.0a3',
+    version='1.0a4.dev0',
     description="TinyMCE plugin integrating iMio's Omnia AI API for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
